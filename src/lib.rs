88de85f6227d02702d453a7d9906e0232cd9ffa7
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-11-16 23:02:27 -0500

    deduplicate code in get_conn

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,13 @@ use diesel::{Connection, RunQueryDsl};
 use diesel::dsl::sql_query;
 
 #[cfg(feature = "prod_db")]
-pub fn get_conn() -> SqliteConnection {
-	let mut conn = SqliteConnection::establish("file:/home/engineer/db.sqlite").unwrap();
-	sql_query("PRAGMA foreign_keys = ON;").execute(&mut conn).unwrap();
-	conn
-}
+const DB: &'static str = "file:/home/engineer/db.sqlite";
 
 #[cfg(not(feature = "prod_db"))]
+const DB: &'static str = "file:db.sqlite";
+
 pub fn get_conn() -> SqliteConnection {
-	let mut conn = SqliteConnection::establish("file:db.sqlite").unwrap();
+	let mut conn = SqliteConnection::establish(DB).unwrap();
 	sql_query("PRAGMA foreign_keys = ON;").execute(&mut conn).unwrap();
 	conn
 }

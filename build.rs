500fc7624e1ac3fa0f5f378813122ac57e223ed0
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-11-17 20:38:26 -0500

    use yarn instead of npm for rebuild-static

--- build.rs
+++ build.rs
@@ -1,18 +1,12 @@
-#[cfg(not(windows))]
-static NPM_COMMAND: &'static str = "npm";
-
-#[cfg(windows)]
-static NPM_COMMAND: &'static str = "npm.cmd";
-
 fn main() {
 	println!("cargo:rerun-if-changed=migrations");
 
 	if cfg!(feature = "rebuild-static") {
 		println!("cargo:rerun-if-changed=frontend");
 
-		let output = std::process::Command::new(NPM_COMMAND)
-			.args(["run", "--prefix", "frontend", "build"])
-			.output().expect("failed to run npm");
+		let output = std::process::Command::new("yarn")
+			.args(["--cwd", "frontend", "run", "build"])
+			.output().expect("failed to run yarn");
 
 		if !output.status.success() {
 			panic!("failed to build frontend");

f1f77bcb55621053acc96b1979f1eef6da8dca5c
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-11-17 20:51:43 -0500

    fix build.rs on Windows

--- build.rs
+++ build.rs
@@ -1,10 +1,16 @@
+#[cfg(not(windows))]
+const YARN_CMD: &'static str = "yarn";
+
+#[cfg(windows)]
+const YARN_CMD: &'static str = "yarn.cmd";
+
 fn main() {
 	println!("cargo:rerun-if-changed=migrations");
 
 	if cfg!(feature = "rebuild-static") {
 		println!("cargo:rerun-if-changed=frontend");
 
-		let output = std::process::Command::new("yarn")
+		let output = std::process::Command::new(YARN_CMD)
 			.args(["--cwd", "frontend", "run", "build"])
 			.output().expect("failed to run yarn");
 

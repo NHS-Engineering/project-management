39e42fedb5c5437e796f211ee909271ca58d26d2
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-11-19 22:24:53 -0500

    refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -21,7 +21,46 @@
 			cargo = nightlyRust.rust;
 			rustc = nightlyRust.rust;
 		};
-		in rec {
+		in let frontend = pkgs.mkYarnPackage {
+			pname = "frontend";
+			version = "0.0.0";
+
+			src = ./frontend;
+
+			buildPhase = "yarn --offline build";
+			distPhase = "true";
+		};
+		in let backend = use_prod_db: rustPlatform.buildRustPackage {
+			pname = "engineering-web-portal";
+			version = "0.1.0";
+			buildInputs = [pkgs.sqlite];
+
+			# disable debugging
+			buildNoDefaultFeatures = true;
+
+			# use production db
+			buildFeatures = pkgs.lib.optional use_prod_db "prod_db";
+
+			src = ./.;
+
+			cargoLock.lockFile = ./Cargo.lock;
+		};
+		in let fullstack = use_prod_db: pkgs.stdenv.mkDerivation {
+			name = "fullstack";
+			src = frontend;
+
+			installPhase = ''
+				mkdir -p $out/bin
+				cp libexec/frontend/deps/frontend/dist $out/frontend -r
+				cp ${backend use_prod_db}/bin/engineering-web-portal $out/bin/backend
+				echo "#!/bin/bash" > $out/bin/fullstack
+				echo "export OVERRIDE_STATIC=$out/frontend" >> $out/bin/fullstack
+				echo "export ROCKET_ADDRESS=0.0.0.0" >> $out/bin/fullstack
+				echo "$out/bin/backend" >> $out/bin/fullstack
+				chmod +x $out/bin/fullstack
+			'';
+		};
+		in {
 			devShells.x86_64-linux.default = pkgs.mkShell {
 				buildInputs = [
 					nightlyRust.rust
@@ -32,42 +71,16 @@
 				];
 			};
 
-			packages.x86_64-linux.default = rustPlatform.buildRustPackage {
-				pname = "engineering-web-portal";
-				version = "0.1.0";
-				buildInputs = [pkgs.sqlite];
-
-				# disable debugging
-				buildNoDefaultFeatures = true;
-
-				# use production db
-				buildFeatures = ["prod_db"];
-
-				src = ./.;
-
-				cargoLock.lockFile = ./Cargo.lock;
-			};
-
-			packages.x86_64-linux.frontend = pkgs.mkYarnPackage {
-				pname = "frontend";
-				version = "0.0.0";
-
-				src = ./frontend;
-
-				buildPhase = "yarn --offline build";
-				distPhase = "true";
-			};
+			packages.x86_64-linux.default = fullstack false;
 
 			deploy.nodes.aws = {
 				hostname = "nhse.zerdle.net";
 				profiles.system = {
 					sshUser = "root";
 					user = "engineer";
-					path = deploy-rs.lib.x86_64-linux.activate.custom packages.x86_64-linux.default ''
+					path = deploy-rs.lib.x86_64-linux.activate.custom (fullstack true) ''
 						screen -XS server quit || true
-						export OVERRIDE_STATIC=${packages.x86_64-linux.frontend}/libexec/frontend/deps/frontend/dist
-						export ROCKET_ADDRESS=0.0.0.0
-						screen -L -Logfile /tmp/server.log -S server -m -d ./bin/engineering-web-portal
+						screen -L -Logfile /tmp/server.log -S server -m -d ./bin/fullstack
 						curl -s http://nhse.zerdle.net:8000
 						curl -s http://nhse.zerdle.net:8000/api/projects/list
 					'';
